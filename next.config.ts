00f870d4a879441955cedcfec18d4638e1075556
Webster <wrmuchefa@gmail.com>
2025-02-20 12:45:37 +0200

    fix: configure output dir

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  distDir: 'out',
   output: 'standalone',
   images: {
     unoptimized: true,
